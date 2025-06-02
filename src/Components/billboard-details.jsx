8cad37d8a8c2539de74fc009c9f6bdf441463625
FunNoLie <bret.malaga@gmail.com>
2025-06-02 22:26:24 +0800

    10pm

--- src/Components/billboard-details.jsx
+++ src/Components/billboard-details.jsx
@@ -3,6 +3,10 @@ import { MapContainer, TileLayer, Marker, Popup, useMapEvents } from 'react-leaf
 import { useLocation } from 'react-router-dom';
 import billboardIMG from '../assets/Vimage.png';
 import locationSample from '../assets/location-tempo.jpg';
+import billboardtempo from '../assets/Vimage.png';
+import billboardGlass from '../assets/billboard-glass.jpg';
+import billboardOutdoor from '../assets/billboard-outdoor.jpg';
+import billboardHill from '../assets/billboard-hill.jpg';
 
 export default function BillboardDetail() {
   const location = useLocation();
@@ -13,10 +17,10 @@ export default function BillboardDetail() {
   return (
     <div className='container-fluid p-5'>
         <div className="container d-flex align-items-center justify-content-center">
-          <img src={billboardIMG} alt="billboard image"  className='w-100'/>
+          <img src={billboardDetails.image} alt="billboard image"  className='w-100'/>
         </div>
         <div className="container d-flex justify-content-center align-items-center" >
-          <h1 className="fw-bold p-2" style={{fontSize: '3rem'}}> {billboardDetails.address} </h1>
+          <h1 className="fw-bold p-2" style={{fontSize: '3rem'}}> {billboardDetails.address} {billboardDetails.name} - {billboardDetails.read}</h1>
         </div>
         <div className="container d-flex p-2" >
           <div className="row">
@@ -36,6 +40,12 @@ export default function BillboardDetail() {
                   <span style={{fontSize: '1.7rem'}}> {billboardDetails.type.toUpperCase()} BILLBOARD </span> 
                 </h3>
                 <br />
+                <h3> <strong>Facing </strong>: 
+                  <span style={{fontSize: '1.7rem'}}> {billboardDetails.facing}</span> 
+                </h3>
+                <h3> <strong>Read </strong>: 
+                  <span style={{fontSize: '1.7rem'}}> {billboardDetails.read}</span> 
+                </h3>
                 <h3> <strong>Dimensions </strong>: 
                   <span style={{fontSize: '1.7rem'}}> {billboardDetails.size.toUpperCase()}</span> 
                 </h3>
--- src/Components/markets.jsx
+++ src/Components/markets.jsx
@@ -5,6 +5,9 @@ import 'leaflet/dist/leaflet.css';
 import mobil from '../assets/mobil.png';
 import { useNavigate } from 'react-router-dom';
 import billboardtempo from '../assets/Vimage.png';
+import billboardGlass from '../assets/billboard-glass.jpg';
+import billboardOutdoor from '../assets/billboard-outdoor.jpg';
+import billboardHill from '../assets/billboard-hill.jpg';
 
 const BillboardMarkets = () => {
 
@@ -67,53 +70,63 @@ const BillboardMarkets = () => {
   const [positions, setPositions] = useState([, 
     {
       id: 1,
-      name: '1st location',
+      name: 'B1',
       description: 'description',
       lat: 32.566336, 
       lang: -86.676552,
-      adddress: 'H88F+G9P Joffre, Alabama, USA',
+      address: 'H88F+G9P Joffre, Alabama, USA',
       size: '10x40',
-      type: 'outdoor'
+      type: 'outdoor',
+      imageL: billboardtempo,
+      imageR: billboardOutdoor
     },
     {
       id: 2,
-      name: '2nd location',
+      name: 'B2',
       description: 'description',
       lat: 32.143310, 
       lang: -86.626482,
-      adddress: '49VF+8CC Gordonville, Alabama, USA',
+      address: '49VF+8CC Gordonville, Alabama, USA',
       size: '10x30',
-      type: 'outdoor'
+      type: 'outdoor',
+      imageL: billboardHill,
+      imageR: billboardHill
     }, 
     {
       id: 3,
-      name: '3rd location',
+      name: 'B3',
       description: 'description',
       lat: 32.532267, 
       lang: -86.332121,
-      adddress: 'GMJ9+W52 Coosada, Alabama, USA',
-      size: '10x40',
-      type: 'outdoor'
+      address: 'GMJ9+W52 Coosada, Alabama, USA',
+      size: '10x20',
+      type: 'outdoor',
+      imageL: billboardGlass,
+      imageR: billboardGlass,
     },
      {
       id: 4,
-      name: '4th location',
+      name: 'B4',
       description: 'description',
       lat: 32.278852, 
       lang: -85.988347,
-      adddress: '72H6+GMR Pike Rd, Alabama, USA',
-      size: '10x40',
-      type: 'outdoor'
+      address: '72H6+GMR Pike Rd, Alabama, USA',
+      size: '10x50',
+      type: 'outdoor',
+      imageL: billboardHill,
+      imageR: billboardOutdoor
     },
      {
       id: 5,
-      name: '5th location',
+      name: 'B5',
       description: 'description',
       lat: 32.622951, 
       lang:-86.227313,
-      adddress: 'JQFF+53M Holtville, Alabama, USA',
-      size: '10x40',
-      type: 'outdoor'
+      address: 'JQFF+53M Holtville, Alabama, USA',
+      size: '10x30',
+      type: 'outdoor',
+      imageL: billboardHill,
+      imageR: billboardOutdoor
     },
   ]);
 
@@ -123,25 +136,51 @@ const BillboardMarkets = () => {
   let bbDetails = [{}];
   const navigate = useNavigate();
 
-  function popUpClickHandler(id){
+  function popUpClickHandler({id, read}){
 
     const billboardId = id;
-    console.log(id);
 
-    positions.map(boards => {
-      if(boards.id === billboardId){
-          bbDetails ={
-            id: boards.id,
-            name: boards.name,
-            description: boards.description,
-            lat: boards.lat,
-            lng: boards.lang,
-            type: boards.type,
-            address: boards.adddress,
-            size: boards.size
-          };
+    
+      if(read === "left"){
+          positions.map(boards => {
+            if(boards.id === billboardId){
+              bbDetails ={
+                id: boards.id,
+                name: boards.name,
+                description: boards.description,
+                lat: boards.lat,
+                lng: boards.lang,
+                type: boards.type,
+                address: boards.address,
+                size: boards.size,
+                read: 'LHR',
+                facing: 'North',
+                image: boards.imageL
+              };
+            }
+          });
+
+        }else{
+          positions.map(boards => {
+            if(boards.id === billboardId){
+              bbDetails ={
+                id: boards.id,
+                name: boards.name,
+                description: boards.description,
+                lat: boards.lat,
+                lng: boards.lang,
+                type: boards.type,
+                address: boards.address,
+                size: boards.size,
+                read: 'RHR',
+                facing: 'South',
+                image: boards.imageR
+              };
+            }
+
+          });
         }
-    });
+
 
 
     navigate('/location-detail', {state: { bbDetails }});
@@ -172,11 +211,34 @@ const BillboardMarkets = () => {
 
             >
               <Popup>
-                <div  onClick={(e) => { e.preventDefault(); popUpClickHandler(boards.id); }} style={{cursor: 'pointer'}}>
-                  <img src={billboardtempo} width='100%' height='100' /> 
+                <div>
+                  {/* <img src={billboardtempo} width='100%' height='100' /> 
                   <h4 className='pt-2 fw-bold'>{ boards.name }</h4>
                   <p> { boards.adddress } </p>
-                  <p> { boards.size } {boards.type}</p>
+                  <p> { boards.size } {boards.type}</p> */}
+                  <div className="container-fluid">
+                      <div className="row">
+                        <div className="col-sm-12 text-center">
+                          <h4 className='pt-2 fw-bold mb-5'>{ boards.address }</h4>
+                        </div>
+                      </div>
+                      <div className="row">
+                          <div className="col-sm-6">
+                              <img src={boards.imageL} width='100%' height='100' onClick={(e) => { e.preventDefault(); popUpClickHandler({ id:boards.id, read:"left"}); }} style={{cursor: 'pointer'}}/>
+                              <div className="fw-bold h5 pt-5 m-0">{boards.name}A - LHR</div> 
+                              <div className='pt-1'><strong>Facing: </strong> North</div>
+                              <div className='pt-0'><strong>Read: </strong> Right Hand</div>
+                              <div className='pt-0'>{boards.type} ( {boards.size} )</div>
+                          </div>
+                          <div className="col-sm-6">
+                              <img src={boards.imageR} width='100%' height='100' onClick={(e) => { e.preventDefault(); popUpClickHandler({ id:boards.id, read:"right"}); }} style={{cursor: 'pointer'}}/>
+                              <div className="fw-bold h5 pt-5 m-0">{boards.name}B - RHR</div> 
+                              <div className='pt-1'><strong>Facing: </strong> South</div>
+                              <div className='pt-0'><strong>Read: </strong> Left Hand</div>
+                              <div className='pt-0'>{boards.type} ( {boards.size} )</div>
+                          </div>
+                      </div>
+                  </div>
                 </div>
               </Popup>
             </Marker>
